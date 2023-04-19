0abf3c3cfd583e5117d41f3823e527d0758f3a62
Sebastian Beltran <sfbedu@gmail.com>
2023-04-19 17:34:40 -0400

    Add ability to delete graphs. Currently not updating in same session

--- main.py
+++ main.py
@@ -39,6 +39,13 @@ def create_graph():
     print(response.text)
 
 
+def delete_graph():
+    selected_graph = [graph for graph in graphs_list if graph["name"] == graph_name.get()]
+    delete_endpoint = f"{graph_endpoint}/{selected_graph[0]['id']}"
+    response = requests.delete(url=delete_endpoint, headers=headers)
+    print(response.text)
+
+
 window = tk.Tk()
 window.title("Pixela Manager")
 window.config(padx=30, pady=60)
@@ -75,8 +82,25 @@ graph_type_input = tk.OptionMenu(window, unit_type, "int", "float")
 graph_type_input.config(bd=2, highlightthickness=0)
 graph_type_input.grid(column=3, row=3, sticky="EW")
 
-create_graph_button = tk.Button(text="Create Graph", command=create_graph)
+create_graph_button = tk.Button(text="Create", command=create_graph)
 create_graph_button.grid(column=2, row=4, pady=20)
+
+delete_graph_label = tk.Label(text="Delete Graph")
+delete_graph_label.grid(column=0, row=5)
+graphs_response = requests.get(url=graph_endpoint, headers=headers)
+graphs_response.raise_for_status()
+graphs_data = graphs_response.json()
+graphs_list = graphs_data["graphs"]
+graphs_names = [graph["name"] for graph in graphs_list]
+graph_name = tk.StringVar(window)
+graph_name.set(graphs_names[0])
+graph_name_options = tk.OptionMenu(window, graph_name, *graphs_names)
+graph_name_options.config(bd=2, highlightthickness=0)
+graph_name_options.grid(column=1, row=5, sticky="EW")
+
+delete_graph_button = tk.Button(text="Delete", command=delete_graph)
+delete_graph_button.grid(column=2, row=5, pady=20)
+
 
 
 
