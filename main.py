193d50b3d65059760801d407aca49e2690c80393
Sebastian Beltran <sfbedu@gmail.com>
2023-04-20 11:47:04 -0400

    Add basic pixel addition to selected graph

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
 import os
+import json
 import requests
 import random
 import tkinter as tk
+from tkcalendar import DateEntry
+from datetime import date
 
 load_dotenv("./.env")
 USERNAME = os.environ["PIXELA_USR"]
@@ -45,6 +48,20 @@ def delete_graph():
     response = requests.delete(url=delete_endpoint, headers=headers)
     print(response.text)
 
+def add_pixel():
+    selected_graph = [graph for graph in graphs_list if graph["name"] == graph_name.get()]
+    date_selected = date_input.get_date().strftime("%Y%m%d")
+    add_pixel_endpoint = f"{graph_endpoint}/{selected_graph[0]['id']}"
+    pixel_config = {
+        "date": date_selected,
+        "quantity": pixel_units_input.get(),
+        "optionalData": json.dumps(description_input.get()),
+    }
+
+    response = requests.post(url=add_pixel_endpoint, json=pixel_config, headers=headers)
+    print(response.text)
+
+
 
 window = tk.Tk()
 window.title("Pixela Manager")
@@ -85,8 +102,11 @@ graph_type_input.grid(column=3, row=3, sticky="EW")
 create_graph_button = tk.Button(text="Create", command=create_graph)
 create_graph_button.grid(column=2, row=4, pady=20)
 
-delete_graph_label = tk.Label(text="Delete Graph")
-delete_graph_label.grid(column=0, row=5)
+edit_graph_section = tk.Label(text="Graph Manager")
+edit_graph_section.grid(column=0, row=5)
+
+select_graph_label = tk.Label(text="Select Graph")
+select_graph_label.grid(column=0, row=6)
 graphs_response = requests.get(url=graph_endpoint, headers=headers)
 graphs_response.raise_for_status()
 graphs_data = graphs_response.json()
@@ -96,12 +116,27 @@ graph_name = tk.StringVar(window)
 graph_name.set(graphs_names[0])
 graph_name_options = tk.OptionMenu(window, graph_name, *graphs_names)
 graph_name_options.config(bd=2, highlightthickness=0)
-graph_name_options.grid(column=1, row=5, sticky="EW")
+graph_name_options.grid(column=1, row=6, sticky="EW")
 
-delete_graph_button = tk.Button(text="Delete", command=delete_graph)
-delete_graph_button.grid(column=2, row=5, pady=20)
+delete_graph_button = tk.Button(text="Delete Graph", command=delete_graph)
+delete_graph_button.grid(column=3, row=6, pady=20)
+
+pixel_units_label = tk.Label(text="Units:")
+pixel_units_label.grid(column=0, row=7)
+pixel_units_input = tk.Entry()
+pixel_units_input.grid(column=1, row=7)
+date_label = tk.Label(text="Date:")
+date_label.grid(column=2, row=7)
+date_input = DateEntry(window, selectmode='day')
+date_input.grid(column=3, row=7)
+
+description_input = tk.Entry()
+description_input.insert(tk.END, "Optional description")
+description_input.grid(column=0, row=8, columnspan=4, rowspan=2, sticky="NSEW")
 
 
+add_pixel_button = tk.Button(text="Add Pixel", command=add_pixel)
+add_pixel_button.grid(column=3, row=11)
 
 
 
