658d66d43340c8defa83367dcf3068873e43a0e9
Sebastian Beltran <sfbedu@gmail.com>
2023-04-24 12:48:33 -0400

    Keep track of new graphs locally. Reduce amounts of requests to make it faster and more reliable. Basic error handling when making requests

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ TOKEN = os.environ["PIXELA_TK"]
 headers = {
     "X-USER-TOKEN": TOKEN,
 }
-print(USERNAME)
+
 pixela_endpoint = "https://pixe.la/v1/users"
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
@@ -30,6 +30,8 @@ colors = {
     "black": "kuro"
 }
 
+global graphs_dict
+
 
 def create_graph():
     graph_config = {
@@ -39,52 +41,70 @@ def create_graph():
         "type": unit_type.get(),
         "color": colors[color.get()]
     }
-    print(graph_config)
-    response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-    print(response.text)
-    graph_name_input.delete(0, 'end')
-    graph_unit_input.delete(0, 'end')
-    refresh_graphs_options()
+
+    try:
+        response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+        print(response.text)
+    except requests.exceptions.HTTPError:
+        print(response.text)
+        print("Could not add the new graph, try again")
+    else:
+        graphs_dict[graph_config["name"]] = graph_config["id"]
+        graph_name_input.delete(0, 'end')
+        graph_unit_input.delete(0, 'end')
+        refresh_graphs_options()
+
+
 
 
 def delete_graph():
-    selected_graph = [graph for graph in graphs_list if graph["name"] == graph_name.get()]
-    delete_endpoint = f"{graph_endpoint}/{selected_graph[0]['id']}"
-    response = requests.delete(url=delete_endpoint, headers=headers)
-    print(response.text)
-    refresh_graphs_options()
-    update_image("")
+    selected_id = graphs_dict[graph_name.get()]
+    delete_endpoint = f"{graph_endpoint}/{selected_id}"
+    try:
+        response = requests.delete(url=delete_endpoint, headers=headers)
+    except requests.exceptions.HTTPError:
+        print("Could not delete graph, try again")
+    else:
+        graphs_dict.pop(graph_name.get())
+        refresh_graphs_options()
+        update_image("")
 
 
 def add_pixel():
-    graphs_list = get_graphs_list()
-    selected_graph = [graph for graph in graphs_list if graph["name"] == graph_name.get()]
     date_selected = date_input.get_date().strftime("%Y%m%d")
-    add_pixel_endpoint = f"{graph_endpoint}/{selected_graph[0]['id']}"
+    selected_id = graphs_dict[graph_name.get()]
+    add_pixel_endpoint = f"{graph_endpoint}/{selected_id}"
     pixel_config = {
         "date": date_selected,
         "quantity": pixel_units_input.get(),
         "optionalData": json.dumps(description_input.get()),
     }
 
-    response = requests.post(url=add_pixel_endpoint, json=pixel_config, headers=headers)
-    update_image("")
-    pixel_units_input.delete(0, 'end')
-    description_input.delete(0, 'end')
+    try:
+        response = requests.post(url=add_pixel_endpoint, json=pixel_config, headers=headers)
+    except requests.exceptions.HTTPError:
+        print("Could not add pixel, try again")
+    else:
+        update_image("")
+        pixel_units_input.delete(0, 'end')
+        description_input.delete(0, 'end')
 
 
 def load_image():
-    graphs_list = get_graphs_list()
-    selected_graph = [graph for graph in graphs_list if graph["name"] == graph_name.get()]
-    id = selected_graph[0]['id']
-    with open(f"graph.svg", mode="wb") as graph_image:
-        get_graph = requests.get(url=f"{graph_endpoint}/{id}")
-        img = get_graph.content
-        graph_image.write(img)
-        source = pyvips.Source.new_from_file("./graph.svg")
-        png_img = pyvips.Image.new_from_source(source, "", dpi=72)
-        target = pyvips.Target.new_to_file(f"./graph.png")
-        png_img.write_to_target(target, ".png")
+    selected_id = graphs_dict[graph_name.get()]
+    print(f"Selected id {selected_id}")
+    try:
+        get_graph = requests.get(url=f"{graph_endpoint}/{selected_id}")
+    except requests.exceptions.HTTPError:
+        print("Could not get image")
+    else:
+        with open(f"graph.svg", mode="wb") as graph_image:
+            img = get_graph.content
+            graph_image.write(img)
+            source = pyvips.Source.new_from_file("./graph.svg")
+            png_img = pyvips.Image.new_from_source(source, "", dpi=72)
+            target = pyvips.Target.new_to_file(f"./graph.png")
+            png_img.write_to_target(target, ".png")
 
 
 def update_image(value):
@@ -97,18 +117,22 @@ def get_graphs_list():
     graphs_response = requests.get(url=graph_endpoint, headers=headers)
     graphs_response.raise_for_status()
     graphs_data = graphs_response.json()
-    return graphs_data["graphs"]
+    response_dict = {}
+    for graph in graphs_data["graphs"]:
+        response_dict[graph["name"]] = graph["id"]
+    return response_dict
 
 
 def refresh_graphs_options():
+    new_graphs_names = list(graphs_dict.keys())
     graph_name_options['menu'].delete(0, 'end')
-    new_graphs_names = [graph["name"] for graph in get_graphs_list()]
     graph_name.set(new_graphs_names[0])
 
     for graph in new_graphs_names:
         graph_name_options['menu'].add_command(label=graph, command=tk._setit(graph_name, graph, update_image))
 
 
+graphs_dict = get_graphs_list()
 
 window = tk.Tk()
 window.title("Pixela Manager")
@@ -116,7 +140,7 @@ window.config(padx=30, pady=60)
 
 title_label = tk.Label(text="Pixela Manager", font=("Arial", 20, "bold"))
 title_label.place(x=115, y=-40)
-
+# Create Section
 create_section_label = tk.Label(text="Create Graph")
 create_section_label.grid(column=0, row=1)
 
@@ -149,14 +173,16 @@ graph_type_input.grid(column=3, row=3, sticky="EW")
 create_graph_button = tk.Button(text="Create", command=create_graph)
 create_graph_button.grid(column=2, row=4, pady=20)
 
+# Edit Section
 edit_graph_section = tk.Label(text="Graph Manager")
 edit_graph_section.grid(column=0, row=5)
 
 select_graph_label = tk.Label(text="Select Graph")
 select_graph_label.grid(column=0, row=6)
 
-graphs_list = get_graphs_list()
-graphs_names = [graph["name"] for graph in graphs_list]
+# graphs_list = []
+graphs_names = list(graphs_dict.keys())
+print(graphs_names)
 graph_name = tk.StringVar(window)
 graph_name.set(graphs_names[0])
 graph_name_options = tk.OptionMenu(window, graph_name, *graphs_names, command=update_image)
