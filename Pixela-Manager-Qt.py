945f2b5419f792ec0ed710028819e2032c61fe20
Sebastian Beltran <sfbedu@gmail.com>
2023-11-16 13:39:04 -0500

    Update implementation
    -Migrating to use PyQt allows faster iteration of UI.
    -Basic graph creation functionality implemented.
    -Changed graph id creation because uuid is too long and not supported by api.

--- Pixela-Manager-Qt.py
+++ Pixela-Manager-Qt.py
@@ -1,7 +1,21 @@
 import sys
+import os
+from dotenv import load_dotenv
+import random
+import requests
 from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6 import uic
-import uuid
+
+load_dotenv("./.env")
+USERNAME = os.environ["PIXELA_USR"]
+TOKEN = os.environ["PIXELA_TK"]
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+pixela_endpoint = "https://pixe.la/v1/users"
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 class Main(QWidget):
     def __init__(self):
@@ -9,6 +23,8 @@ class Main(QWidget):
 
         # import ui from qt designer
         self.ui = uic.loadUi("Pixela-Manager-Qt_v001.ui", self)
+
+        self.graph_color = ""
 
         # connect radio buttons
         self.ui.green_color_picker.toggled.connect(self.get_graph_color)
@@ -23,13 +39,19 @@ class Main(QWidget):
 
     def create_graph(self):
         graph_config = {
-            "id": "graph-" + str(uuid.uuid4()),
+            "id": "graph" + str(random.randint(1, 200)),
             "name": self.ui.graph_name_input.text(),
             "unit": self.ui.unit_name_input.text(),
             "type": self.get_unit_type(),
-            "color": self.get_graph_color()
+            "color": self.graph_color
         }
         print(graph_config)
+        try:
+            response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+            print(response.text)
+        except requests.exceptions.HTTPError:
+            print(response.text)
+            print("Could not add the new graph, try again")
 
     def get_unit_type(self):
         if self.ui.unit_type_menu.currentText() == "Decimal":
@@ -41,18 +63,17 @@ class Main(QWidget):
         selected_color = self.sender()
         if selected_color.isChecked():
             if selected_color == self.ui.green_color_picker:
-                print("Green Color Selected")
-                return "shibafu"
-            if selected_color == self.ui.red_color_picker:
-                return "momiji"
-            if selected_color == self.ui.blue_color_picker:
-                return "sora"
-            if selected_color == self.ui.yellow_color_picker:
-                return "ichou"
-            if selected_color == self.ui.purple_color_picker:
-                return "ajisai"
-            if selected_color == self.ui.kuro_color_picker:
-                return "kuro"
+                self.graph_color = "shibafu"
+            elif selected_color == self.ui.red_color_picker:
+                self.graph_color = "momiji"
+            elif selected_color == self.ui.blue_color_picker:
+                self.graph_color = "sora"
+            elif selected_color == self.ui.yellow_color_picker:
+                self.graph_color = "ichou"
+            elif selected_color == self.ui.purple_color_picker:
+                self.graph_color = "ajisai"
+            elif selected_color == self.ui.kuro_color_picker:
+                self.graph_color = "kuro"
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
