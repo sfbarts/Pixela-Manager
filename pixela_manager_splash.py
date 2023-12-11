d98cc62549d0e8b7cbba39f9162b2c68314bc8c1
Sebastian Beltran <sfbedu@gmail.com>
2023-12-11 10:51:30 -0500

    Update splash screen logic

--- pixela_manager_splash.py
+++ pixela_manager_splash.py
@@ -1,11 +1,12 @@
 from PyQt6.QtWidgets import QSplashScreen
 from PyQt6.QtCore import Qt
 from PyQt6 import uic
+from resource_path import *
 
 class SplashScreen(QSplashScreen):
     def __init__(self):
         super().__init__()
-        self.ui = uic.loadUi("Pixela-Splash-Qt_v001.ui", self)
+        self.ui = uic.loadUi(resource_path("./ui_files/Pixela-Splash-Qt_v001.ui"), self)
         self.setWindowFlag(Qt.WindowType.FramelessWindowHint)
 
 
